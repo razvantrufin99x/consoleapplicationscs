3d383b1a9b5b155220b1c87f4be05a2529fcc332
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-11-28 20:48:19 +0200

    Add project files.

---
+++ ConsoleApplication1.exe/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Untitled.ConsoleApplication1.exe
+{
+	internal class Program
+	{
+		public Program()
+		{
+			//throw new NotImplementedException();
+		}
+
+		private static void Main(String[] args)
+		{
+			//throw new NotImplementedException();
+            Console.Out.WriteLine("iesire");
+            Console.ReadKey();
+		}
+	}
+}
